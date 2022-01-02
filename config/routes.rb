f069667cd80a8c4671befbf30bbd285376382662
Andrew Davisson <akdavisson4@gmail.com>
2022-01-02 11:24:11 -0700

    set up user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root "main#index"
+
   get "/", to: "main#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/users", to: "user#index"
 end
