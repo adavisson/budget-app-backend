2ba8c19f470eb47233acf7c8615a147a871fa7ca
Andrew Davisson <akdavisson4@gmail.com>
2022-01-02 11:46:00 -0700

    user create route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,4 +3,17 @@ class UserController < ApplicationController
     @users = User.all
     render :json => @users
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render :json => "success"
+    else
+      render :json => "failed to create user"
+    end
+  end
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get "/", to: "main#index"
 
   get "/users", to: "user#index"
+  post "/users", to: "user#create"
 end
