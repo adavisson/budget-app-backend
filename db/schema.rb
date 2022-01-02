36400bdb71a22e88b3580bd9be44a2813f779376
Andrew Davisson <akdavisson4@gmail.com>
2022-01-02 16:12:45 -0700

    add relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_02_213214) do
+ActiveRecord::Schema.define(version: 2022_01_02_230610) do
 
   create_table "bills", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2022_01_02_213214) do
     t.string "color"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
