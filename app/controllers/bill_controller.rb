55624d98d0263f986b9331e9c3153c6968a6e779
Andrew Davisson <akdavisson4@gmail.com>
2022-01-02 15:21:56 -0700

    basic bill actions

--- app/controllers/bill_controller.rb
+++ app/controllers/bill_controller.rb
@@ -1,2 +1,19 @@
 class BillController < ApplicationController
+  def index
+    @bills = Bill.find_by(budget_id: params["budget_id"])
+
+    render json: @bills, include: ["category"]
+  end
+
+  def create
+    @bill = Bill.new(bill_params)
+    if @bill.save
+      render json: @bill, include: ["category"]
+    else
+      render :json => "failed to create bill", status: 400
+  end
+
+  def bill_params
+    params.permit(:name, :amount, :budget_id, :category_id)
+  end
 end
