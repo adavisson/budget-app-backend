9623c012550afbf8dc2d57e468b575ee31570b50
Andrew Davisson <akdavisson4@gmail.com>
2022-01-02 15:09:02 -0700

    basic actions for budget

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -1,2 +1,20 @@
 class BudgetController < ApplicationController
+  def create
+    @budget = Budget.new(budget_params)
+    if @budget.save
+      render :json => "success"
+    else
+      render :json => "failed to create budget", status: 400
+    end
+  end
+
+  def show
+    @budget = Budget.find(params[:id])
+    
+    render json: @budget, include: ["bills"]
+  end
+
+  def budget_params
+    params.permit(:income, :user_id)
+  end
 end
