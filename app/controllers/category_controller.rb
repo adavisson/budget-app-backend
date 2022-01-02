c8256d92ec117feaaf284630af2483cbcba9e32b
Andrew Davisson <akdavisson4@gmail.com>
2022-01-02 15:37:53 -0700

    basic category actions

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,2 +1,21 @@
 class CategoryController < ApplicationController
+
+  def show
+    @category = Category.find(params[:id])
+
+    render json: @category, include: ["bills"]
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      render json: @category
+    else
+      render :json => "failed to create category", status: 400
+    end
+  end
+
+  def category_params
+    params.permit(:name, :color)
+  end
 end
