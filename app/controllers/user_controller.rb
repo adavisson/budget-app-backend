9bdbb6afd8fe688ee7df60fa8450b1cdfdca06d7
Andrew Davisson <akdavisson4@gmail.com>
2022-01-02 15:22:08 -0700

    return user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,7 @@ class UserController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render :json => "success"
+      render json: @user
     else
       render :json => "failed to create user", status: 400
     end
