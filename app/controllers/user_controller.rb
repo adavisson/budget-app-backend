085cacbe3fff78831eb18bc42ab56efd449ab01f
Andrew Davisson <akdavisson4@gmail.com>
2022-01-08 10:40:23 -0700

    return token and id

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,7 @@ class UserController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user 
+      render json: {id: @user.id, token: token(@user.id)} 
     else
       render :json => "failed to create user", status: 400
     end
