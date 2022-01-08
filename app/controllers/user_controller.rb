5f02021cbf348165ec57799463e100e07c5bbbc9
Andrew Davisson <akdavisson4@gmail.com>
2022-01-08 10:42:36 -0700

    fix variable name

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,7 @@ class UserController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: {id: @user.id, token: token(@user.id)} 
+      render json: {user_id: @user.id, token: token(@user.id)} 
     else
       render :json => "failed to create user", status: 400
     end
